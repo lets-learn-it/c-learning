64decad6a1347c1bb176968b20a15385bc8dff64
Parikshit Patil <patil8698parikshit@gmail.com>
2022-12-20 21:07:34 +0530

    static changed

--- 01-constants-variables-and-data-types/05-static-keyword/main.c
+++ 01-constants-variables-and-data-types/05-static-keyword/main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+int get_next_number() {
+  static int num = 0;
+  return num++;
+}
+
 int main(int argc, char const *argv[]) {
   // this wont work
   extern int age;
@@ -11,6 +16,9 @@ int main(int argc, char const *argv[]) {
 
   // this will work
   extern int pls_call_get_age();
-  printf("%d", pls_call_get_age());
+  printf("%d\n", pls_call_get_age());
+
+  for (int i=0;i<5;i++)
+    printf("%d ", get_next_number());
   return 0;
 }
